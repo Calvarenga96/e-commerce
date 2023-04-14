33ab6ba0e3b5e8f859fffab899e5605357f972bd
Christian Alvarenga <>
2023-04-14 11:43:25 -0400

    Add styles to the search bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import { ComponentsContext } from "../../context/ComponentsContext";
+import styles from "./index.module.css";
 
 const SearchBar = () => {
     const { inputValue, searchProduct } = useContext(ComponentsContext);
 
     return (
-        <section>
+        <section className={styles.inputContainer}>
             <input
                 type="text"
                 placeholder="Search Product..."
                 value={inputValue}
                 onChange={searchProduct}
+                className={styles.input}
             />
         </section>
     );

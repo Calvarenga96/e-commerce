6b66dcd620579077dd4667fbc390b8637c083727
Christian Alvarenga <>
2023-04-14 12:47:44 -0400

    Correction of some styles and creation of loader

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 
 import Product from "../Product";
+import Loader from "../Loader";
 import useProductsInTens from "../../hooks/useProductsInTens";
 
 import styles from "./index.module.css";
@@ -12,16 +13,22 @@ const ProductList = () => {
     const { products } = useProductsInTens();
 
     return (
-        <section className={styles.productsLayout}>
-            {searchingSomeProduct
-                ? productSearched.map((product, index) => (
-                      <Product key={index} product={product} />
-                  ))
-                : products.map((product, index) => (
-                      <Product key={index} product={product} />
-                  ))}
-            {simulateLoadingData && "Loading Data..."}
-        </section>
+        <>
+            <section className={styles.productsLayout}>
+                {searchingSomeProduct
+                    ? productSearched.map((product, index) => (
+                          <Product key={index} product={product} />
+                      ))
+                    : products.map((product, index) => (
+                          <Product key={index} product={product} />
+                      ))}
+            </section>
+            {simulateLoadingData && (
+                <div className={styles.loaderContainer}>
+                    <Loader />
+                </div>
+            )}
+        </>
     );
 };
 
